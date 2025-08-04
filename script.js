85e2df78a85afaa6a9cb8245192c91c2bc88f266
AgentSamegaki <sweetsadness289@gmil.com>
2025-08-04 16:37:19 +0800

    updated js and html

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 // let sections = document.querySelectorAll('.section');
-let navLinks = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll("header nav a");
 
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
-
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
