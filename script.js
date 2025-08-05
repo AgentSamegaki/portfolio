c1c18b9bf1dc6dc64ae770b2f0cda9392ac6a8f2
AgentSamegaki <sweetsadness289@gmil.com>
2025-08-05 17:19:01 +0800

    added night-mode and updated ui and new designs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//js for the hamburger
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 // let sections = document.querySelectorAll('.section');
@@ -6,4 +7,37 @@ let navLinks = document.querySelectorAll("header nav a");
 menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
+};
+
+(function () {
+  emailjs.init("tztS8EMhH-jTypX7j");
+})();
+
+//js for contact me
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm("service_3z9pkw8", "template_5k0p5r8", this).then(
+      function (response) {
+        alert("Message sent successfully!");
+        document.getElementById("contact-form").reset();
+      },
+      function (error) {
+        console.error("Message failed:", error);
+        alert("Something went wrong. Please try again.");
+      }
+    );
+  });
+
+//for dark-theme toggle
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "img/sun.png";
+  } else {
+    icon.src = "img/moon.png";
+  }
 };
